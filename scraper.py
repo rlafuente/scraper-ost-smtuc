374ae5be73e7f470331a3e0a44af7459c33a64d2
Ricardo Lafuente <r@manufacturaindependente.org>
2014-05-15 12:28:45 +0100

    First version of the Coimbra stops scraper

--- scraper.py
+++ scraper.py
@@ -1,23 +1,76 @@
-# This is a template for a Python scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env pytohn
+# -*- coding: utf-8 -*-
+import os
+import json
+import urllib2
+import scraperwiki
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+# TODO:
+# Opção pra omitir a segunda entrada da mesma paragem
+#   é verificável pelo stop_code
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+API_RESOURCE = "stops"
+AGENCY_ID = 8 # SMTUC
+# API_KEY = "bgcrnEOQXmPHpERnWpyoqExCpjPItNBGnKikJQBo"
+API_KEY = "JaNPGAEzmpMrXBraNnfvcGRuKTKaMAgIygKBGKTD"
+API_URL_BASE = 'https://api.ost.pt'
+API_URL = '%s/%s/?agency=%d&withroutes=false&key=%s' % (API_URL_BASE, API_RESOURCE, AGENCY_ID, API_KEY)
+
+os.environ["SCRAPERWIKI_DATABASE_NAME"] = "data.sqlite"
+i = 1
+
+def scrape(local=False):
+    if not local:
+        generate_db_from_api(API_URL)
+    else:
+        generate_db_from_files()
+
+def data2sqlite(data):
+    for o in data['Objects']:
+        scraperwiki.sqlite.save(unique_keys=['id'], table_name="data",
+                                                    data={"id": o["id"], 
+                                                          "stop_code": o["stop_code"],
+                                                          "stop_desc": o["stop_desc"],
+                                                          "stop_name": o["stop_name"],
+                                                          "parent_station": o["parent_station"],
+                                                          "parent_station_id": o["parent_station_id"],
+                                                          "lat": o["point"]["coordinates"][0],
+                                                          "lon": o["point"]["coordinates"][1],
+                                                          })
+
+def generate_db_from_api(url, save_json_files=False):
+    print "Generating SQLite output from the OST.pt API..."
+    global i
+    response = urllib2.urlopen(url)
+    r = response.read()
+    r = open(file, 'r').read()
+    data = json.loads(r)
+    data2sqlite(data)
+
+    # this should only happen on the local scraper, not on Morph
+    if save_json_files:
+        filename = "smtuc-stops-%02d.json" % i
+        f = open(filename, 'w')
+        f.write(r)
+        f.close()
+    i += 1
+
+    # are there more results?
+    if not data['Meta'].get('next_page'):
+        return
+    next_url = API_URL_BASE + data['Meta']['next_page']
+    fetch_page(next_url)
+
+def generate_db_from_files():
+    print "Generating SQLite output from downloaded files..."
+    from glob import glob
+    files = glob("./smtuc*.json")
+    for f in files:
+        r = open(f, 'r').read()
+        data = json.loads(r)
+        data2sqlite(data)
+        print f
+
+scrape()
+# scrape(local=True)
+
